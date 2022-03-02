4957880a748071474417a9ab372b8a6001bc5c82
act99 <dorxm999@gmail.com>
2022-03-02 12:44:12 +0900

    please

--- src/components/KakaoOauth.js
+++ src/components/KakaoOauth.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { history } from "../redux/store";
 import { apis, axiosInstance } from "../shared/api";
 import { setCookie } from "../shared/Cookie";
+import { KAKAO_AUTH_URL } from "../shared/OAuth";
 
 const KakaoOauth = () => {
   // 인가코드
@@ -9,41 +10,53 @@ const KakaoOauth = () => {
   useEffect(() => {
     let code = new URL(window.location.href).searchParams.get("code");
     const kakaoLogin = async () => {
-      await apis
-        .kakaoLogin(code)
-        .then((res) => {
-          console.log(res);
-          setCookie("token", res.headers.authorization);
-          localStorage.setItem("userId", res.data);
-          history.push("/");
-        })
-        .catch((error) => console.log(error));
+      await apis.kakaoLogin(code).then((res) => {
+        setCookie("token", res.headers.authorization);
+        localStorage.setItem("userId", res.data);
+        window.location.href = KAKAO_AUTH_URL;
+      });
     };
     kakaoLogin();
-
-    // apis
-    //   .kakaoLogin(code)
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     const address = response.data.address;
-    //     console.log(response);
-    //     setCookie("userId", response.data.userId);
-    //     // let profileImg = encodeURIComponent(response.data.profileImg);
-    //     // setCookie("userImg", profileImg);
-    //     let name = encodeURIComponent(response.data.nickName);
-    //     setCookie("userName", name);
-    //     setCookie("userToken", `Bearer ${response.data.token}`);
-    //     // console.log(address);
-    //     if (address !== null) {
-    //       history.push("/");
-    //     } else {
-    //       history.push("/login");
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log("에러발생", err);
-    //   });
   }, []);
+
+  // useEffect(() => {
+  //   let code = new URL(window.location.href).searchParams.get("code");
+  //   const kakaoLogin = async () => {
+  //     await apis
+  //       .kakaoLogin(code)
+  //       .then((res) => {
+  //         console.log(res);
+  //         setCookie("token", res.headers.authorization);
+  //         localStorage.setItem("userId", res.data);
+  //         history.push("/");
+  //       })
+  //       .catch((error) => console.log(error));
+  //   };
+  //   kakaoLogin();
+
+  // apis
+  //   .kakaoLogin(code)
+  //   .then((response) => {
+  //     console.log(response.data);
+  //     const address = response.data.address;
+  //     console.log(response);
+  //     setCookie("userId", response.data.userId);
+  //     // let profileImg = encodeURIComponent(response.data.profileImg);
+  //     // setCookie("userImg", profileImg);
+  //     let name = encodeURIComponent(response.data.nickName);
+  //     setCookie("userName", name);
+  //     setCookie("userToken", `Bearer ${response.data.token}`);
+  //     // console.log(address);
+  //     if (address !== null) {
+  //       history.push("/");
+  //     } else {
+  //       history.push("/login");
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log("에러발생", err);
+  //   });
+  // }, []);
 
   return <p>테스트 로딩중</p>;
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,15 +3,13 @@ import LoginCont from "../components/LoginCont.js";
 import { KAKAO_AUTH_URL } from "../shared/OAuth.js";
 
 const Login = (props) => {
-  const clickSocial = () => {
-    console.log("hi");
-    console.log(KAKAO_AUTH_URL);
-    return (window.location.href = KAKAO_AUTH_URL);
+  const kakaoLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
   };
 
   return (
     <div>
-      <LoginCont onClick={clickSocial} />
+      <LoginCont onClick={kakaoLogin} />
     </div>
   );
 };
