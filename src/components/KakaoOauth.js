01cf9970ce25e63eefc687de1f8175d3e31fa459
act99 <dorxm999@gmail.com>
2022-03-04 15:58:20 +0900

    okay

--- src/components/KakaoOauth.js
+++ src/components/KakaoOauth.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { actionCreators as userActions } from "../redux/modules/userReducer";
 import { useDispatch } from "react-redux";
+import styled from "@emotion/styled";
+import LinearProgress from "@mui/material/LinearProgress";
+
 const KakaoOauth = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -8,7 +11,21 @@ const KakaoOauth = () => {
     dispatch(userActions.kakaoLoginDB(code));
   }, []);
 
-  return <p>테스트 로딩중</p>;
+  return (
+    <Wrap>
+      <p>테스트 로딩중</p>
+      <LinearProgress color="success" />
+    </Wrap>
+  );
 };
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default KakaoOauth;
