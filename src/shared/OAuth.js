b6da8961cdc42080d4f58c38458e0576c726862e
act99 <dorxm999@gmail.com>
2022-03-04 12:09:08 +0900

    url clean

--- src/shared/OAuth.js
+++ src/shared/OAuth.js
@@ -1,11 +1,9 @@
 // ** 픽스!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+import url from "./url";
+
 const CLIENT_ID = "dbf70dbcc152160d45ec6ce156a6c37e";
-const REDIRECT_URI =
-  // 로컬호스트
-  "http://localhost:3000/user/kakao/callback";
-// AWS amplify URL
-// "https://chat.d8pzcrluuw660.amplifyapp.com/user/kakao/callback";
+const REDIRECT_URI = url.REDIRECT_URI;
 
 //닉네임 동의 페이지
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
---
+++ src/shared/url.js
@@ -0,0 +1,13 @@
+// ** 테스트
+// const BASE_URL = "http://52.78.96.234:8080";
+// const REDIRECT_URI = "http://localhost:3000/user/kakao/callback"
+// ** 배포
+const BASE_URL = "https://goonzu.shop";
+const REDIRECT_URI =
+  "https://chat.d8pzcrluuw660.amplifyapp.com/user/kakao/callback";
+const url = {
+  BASE_URL,
+  REDIRECT_URI,
+};
+
+export default url;
